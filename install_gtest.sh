ab671260f5af3e5b8fd59ccbc691692abd0f8233
Dhruvesh <dhruvdplayer@gmail.com>
2018-09-10 00:42:11 +0530

    Update install_gtest.sh

--- install_gtest.sh
+++ install_gtest.sh
@@ -12,6 +12,6 @@ sudo make
 
 sudo cp *.a /usr/lib
 
-sudo ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
+sudo ln -s /usr/lib/libgtest.a /usr/local/lib/libgtest.a
 
-sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
+sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/libgtest_main.a
